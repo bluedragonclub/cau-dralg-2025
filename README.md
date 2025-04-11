39033c67fe65c15c4bc203fcafc06ea27165aaee
Daewon Lee <daewon4you@gmail.com>
2025-04-11 10:53:00 +0900

    Publish assignment_01

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## 2025년 중앙대학교 예술공학부 데이터표현및알고리즘(54601)
 
-- [과제#0: 파일 제출 테스트](https://github.com/bluedragonclub/cau-dralg-2025/tree/main/assignment_00)
+- [프로그래밍과제#0: 파일 제출 테스트](https://github.com/bluedragonclub/cau-dralg-2025/tree/main/assignment_00)
+- [프로그래밍과제#1: 배열 함수 구현](https://github.com/bluedragonclub/cau-dralg-2025/tree/main/assignment_01)
+- [프로그래밍과제#2: 작업내역 관리 구현](https://github.com/bluedragonclub/cau-dralg-2025/tree/main/assignment_02)
+- [프로그래밍과제#3: 단일연결리스트 구현](https://github.com/bluedragonclub/cau-dralg-2025/tree/main/assignment_03)
+- [프로그래밍과제#4: 시간 복잡도 분석](https://github.com/bluedragonclub/cau-dralg-2025/tree/main/assignment_04)
+- [프로그래밍과제#5: 정렬 함수 사용](https://github.com/bluedragonclub/cau-dralg-2025/tree/main/assignment_05)
+- [프로그래밍과제#6: 이진탐색트리 구현](https://github.com/bluedragonclub/cau-dralg-2025/tree/main/assignment_06)
---
+++ assignment_01/array_utils.py
@@ -0,0 +1,78 @@
+import os.path as osp
+
+
+# Problem (1)
+def get_max(arr):
+    # Your code...
+    return 0
+
+
+def get_min(arr):
+    # Your code...
+    return 0
+
+
+def get_sum(arr):
+    # Your code...
+    return 0
+
+
+def get_mean(arr):
+    # Your code...
+    return 0
+
+
+def get_median(arr):
+    # Your code...
+    return 0
+
+
+# Problem (2)
+def count_files(paths):
+    """
+    paths: a list that contains directory and file paths.
+    """
+    n_files = 0
+    # Your code...
+    return n_files
+
+
+def filter_fname(fpaths, word):
+    """
+    fpaths: a list that contains file paths.
+    word: a word that is included in file names.
+    """
+    fpaths_filtered = []
+    # Your code...
+    return fpaths_filtered
+
+
+def filter_ext(fpaths, ext):
+    """
+    fpaths: a list that contains file paths.
+    ext: file extension (e.g., "jpg", "png")
+    """
+    fpaths_filtered = []
+    # Your code...
+    return fpaths_filtered
+
+
+def change_fname(fpaths, old_word, new_word):
+    """
+    fpaths: a list that contains file paths.
+    old_word: an old word to be replaced by the new word.
+    new_word: a new word to replace the old word.
+    """
+    fpaths_new = []
+    # Your code...
+    return fpaths_new
+
+
+def change_dpath(fpaths, dpath):
+    """
+    fpaths: a list that contains file paths.
+    dpath: a new directory path.    
+    """
+    fpaths_new = []
+    # Your code...
+    return fpaths_new
